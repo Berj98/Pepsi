374768413f9b8a8d9390349183e7af84f1957bd3
Berj Ghazarian <99949283+Berj98@users.noreply.github.com>
2024-08-04 18:38:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let img = document.querySelector('.img');
+let body = document.querySelector('body');
+
+function pepsi(drink) {
+    img.src = drink;
+}
+
+function colors(color) {
+    body.style.background = color;    
+}
